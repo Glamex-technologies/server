12878a0b60f70665c18aff1d562d2cd9af471f6f
Himanshu Chauhan <himanshuchau423@gmail.com>
2025-08-19 12:28:40 +0530

    changed admin table

--- src/migrations/20250423151241-create-admins.js
+++ src/migrations/20250423151241-create-admins.js
@@ -1,79 +1,76 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('admins', {
+    await queryInterface.createTable("admins", {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true,
-        allowNull: false
+        allowNull: false,
       },
       first_name: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: true,
       },
       last_name: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: true,
       },
       email: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique: true
+        unique: true,
       },
       full_name: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       phone_code: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: true,
       },
       phone_number: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: true,
       },
       role: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'roles',
-          key: 'id'
+          model: "roles",
+          key: "id",
         },
-        onUpdate: 'CASCADE'
+        onUpdate: "CASCADE",
       },
       status: {
         type: Sequelize.TINYINT,
         allowNull: false,
-        defaultValue: 1
+        defaultValue: 1,
       },
       password: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       profile_image: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: true,
       },
-      remember_token: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
+
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('admins');
-  }
+    await queryInterface.dropTable("admins");
+  },
 };
